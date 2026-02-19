19bbe3fcadac0d1a69a608306ca6b02050f1cb53
Arina <arinapereteatcu0@gmail.com>
2026-02-19 19:30:34 +0200

    feat: add Enemy Waves using Control Tracks and Dialogues using Signals

---
+++ Assets/Scripts/PlayerWeapon.cs
@@ -0,0 +1,61 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerWeapon : MonoBehaviour
+{
+    [SerializeField] GameObject[] lasers;
+    [SerializeField] RectTransform crosshair;
+    [SerializeField] Transform targetPoint;
+    [SerializeField] float targetDistance = 100f;
+
+    bool isFiring = false;
+
+    private void Start()
+    {
+        Cursor.visible = false;
+    }
+    private void Update()
+    {
+        ProcessFiring();
+        MoveCrossHair();
+        MoveTargetPoint();
+        AimLasers();
+    }
+
+   
+    public void OnFire(InputValue value)
+    {
+        isFiring = value.isPressed;
+        Debug.Log("Fire");
+    }
+    void ProcessFiring()
+    {
+        foreach (GameObject laser in lasers)
+        {
+            var EmissionModule = laser.GetComponent<ParticleSystem>().emission;
+            EmissionModule.enabled = isFiring;
+        }
+
+    }
+
+    void MoveCrossHair()
+    {
+        crosshair.position = Input.mousePosition;
+    }
+    void MoveTargetPoint()
+    {
+        Vector3 targetPointPosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, targetDistance);
+        targetPoint.position = Camera.main.ScreenToWorldPoint(targetPointPosition);
+    }
+    void AimLasers()
+    { 
+        foreach (GameObject laser in lasers)
+        {
+            Vector3 fireDirection = targetPoint.position - this.transform.position;
+            Quaternion rotationToTarget = Quaternion.LookRotation(fireDirection);
+            laser.transform.rotation = rotationToTarget;
+        }
+    }
+
+}
